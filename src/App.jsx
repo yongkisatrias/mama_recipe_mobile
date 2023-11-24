31174f1aa9708b54804b502eae8d236b1b524e64
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-24 20:24:26 +0700

    [UPDATE] Create search bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,38 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {PaperProvider, Searchbar} from 'react-native-paper';
 
 function App() {
-  return <SafeAreaView></SafeAreaView>;
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = query => setSearchQuery(query);
+
+  return (
+    <PaperProvider>
+      <SafeAreaView>
+        <View style={styles.root}>
+          <Searchbar
+            placeholder="Search Pasta, Bread, etc"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+            style={styles.searchbar}
+            placeholderTextColor={'#B6B6B6'}
+          />
+        </View>
+      </SafeAreaView>
+    </PaperProvider>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  searchbar: {
+    backgroundColor: '#EFEFEF',
+  },
+});
 
 export default App;
