5c3c87f98f879ccda57f987a91bd4949b4e6c8ac
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-11-24 21:39:35 +0700

    [UPDATE] Create new recipe section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-native/no-inline-styles */
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,7 +8,14 @@
  */
 
 import React from 'react';
-import {Image, SafeAreaView, StyleSheet, View} from 'react-native';
+import {
+  ImageBackground,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  ScrollView,
+} from 'react-native';
 import {PaperProvider, Searchbar, Text} from 'react-native-paper';
 
 function App() {
@@ -16,43 +25,64 @@ function App() {
   return (
     <PaperProvider>
       <SafeAreaView>
-        <View style={styles.root}>
-          <Searchbar
-            placeholder="Search Pasta, Bread, etc"
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-            style={styles.searchbar}
-            placeholderTextColor={'#B6B6B6'}
-          />
-          {/* Popular section */}
-          <Text style={styles.popularSection}>Popular for You</Text>
-          <View style={styles.popularImage}>
-            {[
-              {
-                icons: <Image source={require('./assets/assets-1.png')} />,
-                label: 'Soup',
-              },
-              {
-                icons: <Image source={require('./assets/assets-2.png')} />,
-                label: 'Chicken',
-              },
-              {
-                icons: <Image source={require('./assets/assets-3.png')} />,
-                label: 'Seafood',
-              },
-              {
-                icons: <Image source={require('./assets/assets-2.png')} />,
-                label: 'Dessert',
-              },
-            ].map((item, key) => (
-              <View key={key}>
-                {item.icons}
-                <Text style={styles.popularText}>{item.label}</Text>
+        <ScrollView>
+          <View style={styles.root}>
+            <Searchbar
+              placeholder="Search Pasta, Bread, etc"
+              onChangeText={onChangeSearch}
+              value={searchQuery}
+              style={styles.searchbar}
+              placeholderTextColor={'#B6B6B6'}
+            />
+            {/* Popular section */}
+            <Text style={styles.popularSection}>Popular for You</Text>
+            <View style={styles.popularImage}>
+              {[
+                {
+                  icons: <Image source={require('./assets/assets-1.png')} />,
+                  label: 'Soup',
+                },
+                {
+                  icons: <Image source={require('./assets/assets-2.png')} />,
+                  label: 'Chicken',
+                },
+                {
+                  icons: <Image source={require('./assets/assets-3.png')} />,
+                  label: 'Seafood',
+                },
+                {
+                  icons: <Image source={require('./assets/assets-2.png')} />,
+                  label: 'Dessert',
+                },
+              ].map((item, key) => (
+                <View key={key}>
+                  {item.icons}
+                  <Text style={styles.popularText}>{item.label}</Text>
+                </View>
+              ))}
+            </View>
+            {/* Popular section end */}
+
+            {/* New recipe */}
+            <Text style={styles.newRecipesSection}>New Recipes</Text>
+            <ScrollView horizontal>
+              <View style={{flexDirection: 'row'}}>
+                {[...new Array(8)].map((item, key) => (
+                  <View>
+                    <ImageBackground
+                      source={require('./assets/food-1.png')}
+                      resizeMode="cover"
+                      style={styles.newRecipeImage}
+                      key={key}>
+                      <Text style={styles.newRecipeText}>Banana Lemonilo</Text>
+                    </ImageBackground>
+                  </View>
+                ))}
               </View>
-            ))}
+            </ScrollView>
+            {/* New recipe end */}
           </View>
-          {/* Popular section end */}
-        </View>
+        </ScrollView>
       </SafeAreaView>
     </PaperProvider>
   );
@@ -81,6 +111,24 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: 500,
     marginTop: 5,
+  },
+  newRecipesSection: {
+    fontSize: 20,
+    fontWeight: 500,
+    marginTop: 20,
+  },
+  newRecipeImage: {
+    height: 160,
+    width: 130,
+    paddingLeft: 0,
+    paddingBottom: 10,
+    justifyContent: 'flex-end',
+  },
+  newRecipeText: {
+    color: '#fff',
+    padding: 20,
+    fontSize: 14,
+    fontWeight: 500,
   },
 });
 
