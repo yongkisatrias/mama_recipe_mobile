5e2794df28e5e47a4b227a9be31cfc07ebc88880
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-12-03 23:20:02 +0700

    [UPDATE] Instal firestore

--- android/app/src/main/java/com/mama_recipe_mobile/MainApplication.java
+++ android/app/src/main/java/com/mama_recipe_mobile/MainApplication.java
@@ -9,8 +9,10 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import androidx.multidex.MultidexApplication;
+// import io.invertase.firebase.auth.ReactNativeFirebaseAuthPackage;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultidexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new DefaultReactNativeHost(this) {
@@ -25,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          // package.add(new ReactNativeFirebaseAuthPackage())
           return packages;
         }
 
