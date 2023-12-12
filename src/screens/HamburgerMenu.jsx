22ccb4537e2479a286d45182a4928afa5200b662
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-12-12 21:50:21 +0700

    [UPDATE] Update hamburger menu

--- src/screens/HamburgerMenu.jsx
+++ src/screens/HamburgerMenu.jsx
@@ -1,12 +1,80 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  ScrollView,
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/dist/AntDesign';
 
 function HamburgerMenu({navigation}) {
   return (
-    <View>
-      <Text>Test 1</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView style={{backgroundColor: '#EEC302', height: '100%'}}>
+        <View>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Text style={styles.arrowIcon}>
+              <Icon name="arrowleft" size={30} />
+            </Text>
+          </TouchableOpacity>
+          <View style={styles.menuOptions}>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Text style={styles.homeMenu}>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+              <Text style={styles.loginMenu}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+              <Text style={styles.registerMenu}>Register</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('')}>
+              <Text style={styles.profileMenu}>Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('')}>
+              <Text style={styles.logoutMenu}>Logout</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  arrowIcon: {
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  menuOptions: {
+    alignItems: 'center',
+    marginTop: 150,
+  },
+  homeMenu: {
+    fontSize: 30,
+    fontWeight: '600',
+  },
+  loginMenu: {
+    fontSize: 30,
+    marginTop: 15,
+    fontWeight: '600',
+  },
+  registerMenu: {
+    fontSize: 30,
+    marginTop: 15,
+    fontWeight: '600',
+  },
+  profileMenu: {
+    fontSize: 30,
+    marginTop: 15,
+    fontWeight: '600',
+  },
+  logoutMenu: {
+    fontSize: 30,
+    marginTop: 15,
+    fontWeight: '600',
+  },
+});
 
 export default HamburgerMenu;
