068fc71939f33acfae577abe3863c8b36e024b4e
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-12-14 00:22:02 +0700

    [UPDATE] Create header section profile screen

--- src/screens/Profile.screen.jsx
+++ src/screens/Profile.screen.jsx
@@ -1,12 +1,63 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/AntDesign';
 
-function ProfileScreen() {
+function ProfileScreen({navigation}) {
   return (
-    <View>
-      <Text>Test</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.headerSection}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Text style={styles.arrowIcon}>
+              <Icon name="arrowleft" size={35} color="#fff" />
+            </Text>
+          </TouchableOpacity>
+          <Image
+            source={require('../assets/user-profile-dummy.png')}
+            style={styles.profilePicture}
+          />
+          <Text style={styles.profileName}>Mareta Lopeda</Text>
+        </View>
+        <View style={styles.mainSection}></View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  headerSection: {
+    height: 300,
+    backgroundColor: '#EEC302',
+  },
+  mainSection: {
+    height: 400,
+  },
+  arrowIcon: {
+    paddingTop: 15,
+    paddingLeft: 15,
+  },
+  profilePicture: {
+    width: 95,
+    height: 95,
+    borderRadius: 100,
+    marginTop: 30,
+    alignSelf: 'center',
+  },
+  profileName: {
+    fontSize: 22,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginTop: 10,
+    color: '#fff',
+  },
+});
 
 export default ProfileScreen;
